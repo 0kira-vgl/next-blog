e0488579d2e21988bae195b76d0d285984402b4a
matheus <matheusgtiburcio@gmail.com>
2026-02-09 17:02:05 -0300

    feat: add CustomerStory component with layout, styles, and customer testimonials

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { CustomerStory } from "@/components/customerStory";
 import { Feature } from "@/components/feature";
 import { Hero } from "@/components/hero";
 import { Support } from "@/components/support";
@@ -9,6 +10,7 @@ export default function Home() {
         <Hero />
         <Feature />
         <Support />
+        <CustomerStory />
       </article>
     </>
   );
