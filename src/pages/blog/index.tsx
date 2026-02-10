e458429eaf5bb728a85b0edd15d93ccfc9bb26c4
matheus <matheusgtiburcio@gmail.com>
2026-02-10 12:24:51 -0300

    feat(blog): add initial blog page layout with header and title

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,19 @@
+export default function Blog() {
+  return (
+    <div className="flex h-full grow flex-col pt-24">
+      <header>
+        <div className="container flex flex-col items-start justify-between space-y-6 md:flex-row md:items-end lg:items-end">
+          <div className="flex flex-col gap-4">
+            <span className="text-body-tag w-fit rounded-md bg-cyan-300 px-4 py-2 text-center text-cyan-100 uppercase md:text-left">
+              BLOG
+            </span>
+
+            <h1 className="md:text-heading-xl max-w-2xl text-start text-balance text-gray-100 text-shadow-lg md:text-left">
+              Dicas e estratégias para impulsionar seu negócio
+            </h1>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
