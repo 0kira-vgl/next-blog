43c32a85f9a41bb771d5e63939697a4400748cf7
matheus <matheusgtiburcio@gmail.com>
2026-02-10 19:02:29 -0300

    feat: add PostCard and PostGridCard components for blog layout

---
+++ src/components/postCard/index.tsx
@@ -0,0 +1,70 @@
+import Image from "next/image";
+import Link from "next/link";
+
+type Author = {
+  name: string;
+  avatar: string;
+};
+
+type PostCardProps = {
+  slug: string;
+  title: string;
+  description: string;
+  image: string;
+  date: string;
+  author: Author;
+};
+
+export function PostCard({
+  slug,
+  title,
+  description,
+  image,
+  date,
+  author,
+}: PostCardProps) {
+  return (
+    <Link
+      href={`/blog/${slug}`}
+      className="w-full max-w-2xl overflow-hidden rounded-xl border border-gray-400 bg-gray-600 transition-all duration-300 hover:border-blue-300"
+    >
+      <div className="overflow-hidden rounded-md p-2">
+        <div className="relative">
+          <div className="absolute top-0 right-0 rounded-bl-[10px] bg-gray-600 px-3 py-1 backdrop-blur-sm">
+            <span className="text-body-xs text-gray-300">{date}</span>
+          </div>
+          <Image
+            src={image}
+            alt="thumbnail"
+            width={288}
+            height={144}
+            className="h-40 w-full rounded-lg object-cover"
+          />
+        </div>
+
+        <div className="mt-4 space-y-4 px-2">
+          <h2 className="text-heading-sm line-clamp-2 text-gray-100">
+            {title}
+          </h2>
+
+          <p className="text-body-sm line-clamp-3 text-gray-300">
+            {description}
+          </p>
+
+          <div className="flex items-center gap-3 border-t border-gray-400 py-4">
+            <div className="relative size-5 overflow-hidden rounded-full border border-blue-200 md:size-6">
+              <Image
+                src={author.avatar}
+                alt="user-avatar"
+                fill
+                className="rounded-md object-cover"
+              />
+            </div>
+
+            <span className="text-body-sm text-gray-300">{author.name}</span>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+}
--- src/templates/blog/index.tsx
+++ src/templates/blog/index.tsx
@@ -1,3 +1,5 @@
+import { PostCard } from "@/components/postCard";
+import { PostGridCard } from "@/components/postGridCard";
 import { Search } from "@/components/search";
 import { useRouter } from "next/router";
 
@@ -10,7 +12,7 @@ export function BlogList() {
 
   return (
     <div className="flex h-full grow flex-col pt-24">
-      <header>
+      <header className="pb-14">
         <div className="container flex flex-col items-start justify-between space-y-6 md:flex-row md:items-end lg:items-end">
           <div className="flex flex-col gap-4">
             <span className="text-body-tag w-fit rounded-md bg-cyan-300 px-4 py-2 text-center text-cyan-100 uppercase md:text-left">
@@ -25,6 +27,20 @@ export function BlogList() {
           <Search />
         </div>
       </header>
+
+      <PostGridCard>
+        <PostCard
+          slug="/transformando"
+          title="Transformando seu negócio em uma loja virtual"
+          description="Se você está buscando uma maneira simples e eficaz de vender seus produtos online, o Site..."
+          image="/assets/first-post.jpg"
+          date="10/12/24"
+          author={{
+            avatar: "/assets/customer-01.png",
+            name: "Aspen Dokidis",
+          }}
+        />
+      </PostGridCard>
     </div>
   );
 }
