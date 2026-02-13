72790b7bdbb5321778832cdb9154ed48451f2b6b
matheus <matheusgtiburcio@gmail.com>
2026-02-13 15:09:22 -0300

    feat(blog): add new blog post about Site.Set benefits

--- src/templates/blog/index.tsx
+++ src/templates/blog/index.tsx
@@ -2,6 +2,7 @@ import { PostCard } from "@/components/postCard";
 import { PostGridCard } from "@/components/postGridCard";
 import { Search } from "@/components/search";
 import { allPosts } from "contentlayer/generated";
+import { Inbox } from "lucide-react";
 import { useRouter } from "next/router";
 
 export function BlogList() {
@@ -11,42 +12,61 @@ export function BlogList() {
     ? `Resultados de busca para: "${query}"`
     : "Dicas e estratégias para impulsionar seu negócio";
 
-  const posts = allPosts;
+  const posts = query
+    ? allPosts.filter((post) =>
+        post.title.toLowerCase()?.includes(query.toLowerCase()),
+      )
+    : allPosts;
+
+  const hasPosts = posts.length > 0;
 
   return (
     <div className="flex h-full grow flex-col pt-24">
       <header className="pb-14">
         <div className="container flex flex-col items-start justify-between space-y-6 md:flex-row md:items-end lg:items-end">
-          <div className="flex flex-col gap-4">
-            <span className="text-body-tag w-fit rounded-md bg-cyan-300 px-4 py-2 text-center text-cyan-100 uppercase md:text-left">
+          <div className="flex flex-col gap-4 md:px-0">
+            <span className="text-body-tag w-fit rounded-md bg-cyan-300 px-4 py-2 text-center text-cyan-100 md:text-left">
               BLOG
             </span>
 
-            <h1 className="md:text-heading-xl max-w-2xl text-start text-balance text-gray-100 text-shadow-lg md:text-left">
+            <h1 className="text-heading-lg md:text-heading-xl max-w-2xl text-start text-balance text-gray-100 md:text-left">
               {pageTitle}
             </h1>
           </div>
-
           <Search />
         </div>
       </header>
 
-      <PostGridCard>
-        {posts.map((post) => (
-          <PostCard
-            key={post._id}
-            slug={post.slug}
-            title={post.title}
-            description={post.description}
-            image={post.image}
-            date={new Date(post.date).toLocaleDateString("pt-BR")}
-            author={{
-              name: post.author.name,
-              avatar: post.author.avatar,
-            }}
-          />
-        ))}
-      </PostGridCard>
+      {hasPosts && (
+        <PostGridCard>
+          {posts.map((post) => (
+            <PostCard
+              key={post._id}
+              slug={post.slug}
+              title={post.title}
+              description={post.description}
+              image={post.image}
+              date={new Date(post.date).toLocaleDateString("pt-BR")}
+              author={{
+                name: post.author.name,
+                avatar: post.author.avatar,
+              }}
+            />
+          ))}
+        </PostGridCard>
+      )}
+
+      {!hasPosts && (
+        <div className="container px-8">
+          <div className="flex flex-col items-center justify-center gap-8 rounded-lg border-2 border-dashed border-gray-300/40 p-8 md:p-12">
+            <Inbox className="size-12 text-cyan-100" />
+
+            <span className="text-center text-gray-100">
+              Nenhum post encontrado :(
+            </span>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
