ad88485fa5385de9aef319e4793ac865681369aa
matheus <matheusgtiburcio@gmail.com>
2026-02-16 14:49:40 -0300

    feat(avatar): implement Avatar component with Container, Content, Description, Image, and Title subcomponents feat(blog): enhance PostPage with avatar integration and improved post details display fix(breadcrumb): correct formatting and syntax in Breadcrumb component

---
+++ src/components/avatar/index.ts
@@ -0,0 +1,13 @@
+import { AvatarContainer } from "./avatarContainer";
+import { AvatarContent } from "./avatarContent";
+import { AvatarDescription } from "./avatarDescription";
+import { AvatarImage } from "./avatarImage";
+import { AvatarTitle } from "./avatarTitle";
+
+export const Avatar = {
+  Container: AvatarContainer,
+  Content: AvatarContent,
+  Image: AvatarImage,
+  Description: AvatarDescription,
+  Title: AvatarTitle,
+};
